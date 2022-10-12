7651422626c0ab70b03b0c86431bb8bef68ee0c9
jgiordano20191 <jgiordano20191@gmail.com>
2022-10-12 12:14:18 -0700

    Update database.tf

--- modules/database/database.tf
+++ modules/database/database.tf
@@ -1,1 +1,33 @@
+variable "resource_group_name" {
+  type = string
+}
 
+variable "location" {
+  type = string
+}
+
+variable "username" {
+  sensitive = true
+  type = string
+}
+
+variable "password" {
+  sensitive = true
+  type = string
+}
+
+resource "azurerm_sql_server" "terraform-sqlserver" {
+  name                         = "terraformsqlserver"
+  resource_group_name          = var.resource_group_name
+  location                     = var.location
+  version                      = "12.0"
+  administrator_login          = var.user
+  administrator_login_password = var.password
+}
+
+resource "azurerm_sql_database" "terraform-demo-sql-database" {
+  name                = "terraform-demo-sqldb"
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  server_name         = azurerm_sql_server.terraform-sqlserver.name
+}
