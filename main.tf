42014494a283965ef65ab5cd316196977bc76ca6
jgiordano20191 <jgiordano20191@gmail.com>
2022-10-12 13:05:14 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,12 @@
-
+terraform {
+  required_providers  {
+      azurerm = {
+         source =  "hashicorp/azurerm"
+         version = "2.0"
+      }
+  }
+  
+backend "azurerm"  {
+  resource_group_name  =  "terraform-backend"
+  storage_account_name =  "terraformbackenddemo"
+  container_name       =  "tfstate"
